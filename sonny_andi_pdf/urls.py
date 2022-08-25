69027d9bf9f36be4d64efe784d4ea88e87fcd63d
Sonny Wandel <sonny.wandel@gmx.de>
2022-08-25 22:56:52 +0200

    Base HTML + CSS

--- sonny_andi_pdf/urls.py
+++ sonny_andi_pdf/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from sonny_andi_pdf.views import prototype, dummy_addition
+from sonny_andi_pdf.views import prototype, dummy_addition, home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', prototype, name='prototype'),
-    path('', dummy_addition),
+    #path('', prototype, name='prototype'),
+    path('dummy_addition/', dummy_addition),
+    path('', home_view, name='home'),
 ]
--- sonny_andi_pdf/views.py
+++ sonny_andi_pdf/views.py
@@ -6,7 +6,15 @@ from .services import add
 def prototype(request):
     return HttpResponse("Tagesziel erreicht! Zeit für Bier.")
 
+
 def dummy_addition(request):
     a = 1
     b = 1
-    return HttpResponse(f"Addition of a:{a} and b:{b} equals {add(a,b)}")
+    return HttpResponse(f"Addition of a:{a} and b:{b} equals {add(a, b)}")
+
+
+# /home
+def home_view(request):
+    return render(request, 'sonny_andi_pdf/home.html')
+
+# C:\projects\sonny_andi_pdf\templates\sonny_andi_pdf\home.html
